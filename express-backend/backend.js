94282266918c67e2d63480ba0e08a5407a60d853
Brett Gowling <bgowling@calpoly.edu>
2022-04-05 16:25:50 -0700

    added hard DELETE & GET for name+job or just job

--- express-backend/backend.js
+++ express-backend/backend.js
@@ -41,8 +41,19 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
     const name = req.query.name;
-    if (name != undefined){
+    const job = req.query.job;
+    if ((name != undefined) && (job != undefined)){
+        let result = findUserByNameAndJob(name,job);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else if (name != undefined){
         let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else if (job != undefined){
+        let result = findUserByJob(job);
         result = {users_list: result};
         res.send(result);
     }
@@ -71,6 +82,14 @@ const findUserByName = (name) => {
     return users['users_list'].filter( (user) => user['name'] === name); 
 }
 
+const findUserByJob = (job) => { 
+    return users['users_list'].filter( (user) => user['job'] === job); 
+}
+
+const findUserByNameAndJob = (name,job) => { 
+    return users['users_list'].filter( (user) => (user['name'] === name) && (user['job'] === job) ); 
+}
+
 app.post('/users', (req, res) => {
     const userToAdd = req.body;
     addUser(userToAdd);
@@ -79,6 +98,23 @@ app.post('/users', (req, res) => {
 
 function addUser(user){
     users['users_list'].push(user);
+}
+
+app.delete('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        result = {users_list: result};
+        delUser(result);
+        res.status(200).end();
+    }
+});
+
+function delUser(user){
+    const index = users['users_list'].indexOf(user);
+    users['users_list'].splice(index, 1);
 }
 
 app.listen(port, () => {
