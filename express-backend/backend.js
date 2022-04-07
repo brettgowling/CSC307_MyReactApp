e31d78bd1a050f1878067d64eebefa16200ff6b5
Brett Gowling <bgowling@calpoly.edu>
2022-04-07 16:21:25 -0700

    POST now returns code 201, and frontend updates appropriately

--- express-backend/backend.js
+++ express-backend/backend.js
@@ -96,7 +96,7 @@ const findUserByNameAndJob = (name,job) => {
 app.post('/users', (req, res) => {
     const userToAdd = req.body;
     addUser(userToAdd);
-    res.status(200).end();
+    res.status(201).end();
 });
 
 function addUser(user){
